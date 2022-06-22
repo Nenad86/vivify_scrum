1e86ab26ec69849beba900cff21e8d2927f1cf9e
nenad.damjanovic <ndamjanovic@lendflow.com>
2022-06-22 10:21:31 +0200

    env plugin added

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,3 +20,23 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+const fs = require('fs-extra');
+const path = require('path');
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress/config', `${file}.json`);
+
+  return fs.readJson(pathToConfigFile);
+}
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+module.exports = (on, config) => {
+
+  const file = config.env.configFile;
+
+  return getConfigurationByFile(file);
+};
